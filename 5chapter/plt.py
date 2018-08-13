0d269e6bf385e132a5d000caa2d8aacbfe59dcb4
ThanatosSchonan <35918242+ThanatosSchonan@users.noreply.github.com>
2018-08-13 10:02:50 +0800

    sururu
    sururu

---
+++ 5chapter/plt.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+np.random.seed(1000)
+y = np.random.standard_normal(20)
+x = range(len(y))
+plt.plot(x,y)
+plt.show()
+plt.plot(y.cumsum())
+plt.show()
